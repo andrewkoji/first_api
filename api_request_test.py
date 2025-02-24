0a072667e492afd67dbca6c2e6c9c09c6f933e38
alevi <alevinton151@gmail.com>
2025-02-23 22:35:26 -0500

    check on memory

--- api_request_test.py
+++ api_request_test.py
@@ -1,8 +1,8 @@
 import requests
 
-base_url = 'http://127.0.0.1:5000/duplicate'
+base_url = 'https://first-api-y6hb.onrender.com/lowercase'
 
-params = {'text':'hello world', 'times': 42, 'case': 'UPPER'}
+params = {'text':'summarize this email: After seeing that Williamsville is no. 1 rated school district: Another strategy is to buy a starter home in Williamsville. Yes smaller in size, but mortgage would be less and upside for value could be good enough to resell with ease at a profit. Also have lower mortgage payment. also money available for small renovations. Downside: house size for ones I found is ca 2000 sq feet (like our house). Know nothing about addresses, but they look ok. You might have to think of probably three bedrooms (ok while you have one child of course, but dont have twins!).'}
 
 response = requests.get(base_url, params=params)
 
